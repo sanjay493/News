fd02df7f278ddcb1f3be11902a21e81e7a5233a7
sanjay493 <sanjayism@gmail.com>
2019-05-11 19:05:56 +0530

    quiz add

---
+++ public/js/question.js
@@ -0,0 +1,43 @@
+var question = [{
+    "question" : "Who is the 1st prime Mininster of India?",
+    "option1" : "Jawahar lal Nehru",
+    "option2" : "Motilal Nehru",
+    "option3" : "Arun Nehru",
+    "option4" : "Narendra Modi",
+    "answer" :"1"
+},
+{
+"question" : "Who is the 1st prime Mininster of India?",
+"option1" : "Jawahar lal Nehru",
+"option2" : "Motilal Nehru",
+"option3" : "Arun Nehru",
+"option4" : "Narendra Modi",
+"answer" :"1"
+},
+{
+    "question" : "Who is the 1st prime Mininster of India?",
+    "option1" : "Motilal Nehru",
+    "option2" : "Jawahar lal Nehru",
+    "option3" : "Arun Nehru",
+    "option4" : "Narendra Modi",
+    "answer" :"2"
+    },
+    {
+        "question" : "Who is the 1st prime Mininster of India?",
+        "option1" : "Jawahar lal Nehru",
+        "option2" : "Motilal Nehru",
+        "option3" : "Arun Nehru",
+        "option4" : "Narendra Modi",
+        "answer" :"1"
+        },
+        {
+            "question" : "Who is the 1st prime Mininster of India?",
+            "option1" : "Jawahar lal Nehru",
+            "option2" : "Motilal Nehru",
+            "option3" : "Arun Nehru",
+            "option4" : "Narendra Modi",
+            "answer" :"1"
+            }
+
+
+]
