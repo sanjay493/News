448627c6ede609e5a854e42b2404534a0e89caf0
sanjay493 <sanjayism@gmail.com>
2019-05-12 06:09:28 +0530

    startup updates

--- public/js/question.js
+++ public/js/question.js
@@ -27,17 +27,42 @@ var question = [{
         "option1" : "Bill Gates",
         "option2" : "Steve Jobs",
         "option3" : "Mark Zuckerberg",
-        "option4" : "Narendra Modi",
+        "option4" : "Ryan Dahl",
         "answer" :"4"
         },
         {
             "question" : "Who is the father of Python?",
-            "option1" : "Jawahar lal Nehru",
+            "option1" : "Guido van Rossum",
             "option2" : "Motilal Nehru",
             "option3" : "Arun Nehru",
             "option4" : "Narendra Modi",
             "answer" :"1"
-            }
-
+            },
+            {
+                "question" : "Who is the father of JavaScript?",
+                "option1" : "Guido van Rossum",
+                "option2" : "Motilal Nehru",
+                "option3" : "Brendan Eich",
+                "option4" : "Narendra Modi",
+                "answer" :"3"
+                },
+                {
+                    "question" : "Who is the father of Java?",
+                    "option1" : "Guido van Rossum",
+                    "option2" : "James Gosling",
+                    "option3" : "Brendan Eich",
+                    "option4" : "Narendra Modi",
+                    "answer" :"2"
+                    },
+                    {
+                        "question" : "Who is the father of  C Language?",
+                        "option1" : "Guido van Rossum",
+                        "option2" : "James Gosling",
+                        "option3" : "Brendan Eich",
+                        "option4" : "Dennis MacAlistair Ritchie",
+                        "answer" :"4"
+                        }
+    
+    
 
 ]
